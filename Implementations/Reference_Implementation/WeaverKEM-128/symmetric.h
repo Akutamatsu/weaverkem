d3fee6f5d23e1f7c8f0a5f776b529917e41f4817
csfight <614507281@qq.com>
2026-06-24 23:21:38 +0800

    Add files via upload

--- Implementations/Reference_Implementation/WeaverKEM-128/symmetric.h
+++ Implementations/Reference_Implementation/WeaverKEM-128/symmetric.h
@@ -9,11 +9,15 @@
 #include <stdint.h>
 #include "params.h"
 
+#if WEAVER_MODE == 1
 #define XOF_BLOCKBYTES 168
+#else
+#define XOF_BLOCKBYTES 136
+#endif
 #define SHAKE256_RATE 136
 
 typedef struct {
-  uint8_t msg[34];
+  uint8_t msg[WEAVER_SYMBYTES + 2];
   size_t msg_len;
   size_t pos;
 } xof_state;
@@ -33,6 +37,9 @@ void weaver_iccs_hash_h(uint8_t *out, const uint8_t *in, size_t inlen);
 #define weaver_iccs_hash_g WEAVER_NAMESPACE(weaver_iccs_hash_g)
 void weaver_iccs_hash_g(uint8_t *out, const uint8_t *in, size_t inlen);
 
+#define weaver_iccs_expand_keypair_seeds WEAVER_NAMESPACE(weaver_iccs_expand_keypair_seeds)
+void weaver_iccs_expand_keypair_seeds(uint8_t *out, const uint8_t *in, size_t inlen);
+
 #define weaver_iccs_hash_kr WEAVER_NAMESPACE(weaver_iccs_hash_kr)
 void weaver_iccs_hash_kr(uint8_t *out, size_t outlen, const uint8_t *in, size_t inlen);
 
@@ -46,6 +53,7 @@ void weaver_iccs_rkprf(uint8_t out[WEAVER_SSBYTES],
 
 #define hash_h(OUT, IN, INBYTES) weaver_iccs_hash_h(OUT, IN, INBYTES)
 #define hash_g(OUT, IN, INBYTES) weaver_iccs_hash_g(OUT, IN, INBYTES)
+#define expand_keypair_seeds(OUT, IN, INBYTES) weaver_iccs_expand_keypair_seeds(OUT, IN, INBYTES)
 #define xof_absorb(STATE, SEED, X, Y) weaver_iccs_xof_absorb_wrap(STATE, SEED, X, Y)
 #define xof_squeezeblocks(OUT, OUTBLOCKS, STATE) weaver_iccs_xof_squeezeblocks_wrap(OUT, OUTBLOCKS, STATE)
 #define prf(OUT, OUTBYTES, KEY, NONCE) weaver_iccs_prf(OUT, OUTBYTES, KEY, NONCE)
